aae0e17d4b3688057ac7ae8d7850af6da09f00f7
John McNamara <johnmcnamara@Johns-Mac-mini.local>
2017-04-05 21:56:11 -0700

    add from INITIALIZR

---
+++ src/main/java/com/cdsk/DataingestApplication.java
@@ -0,0 +1,12 @@
+package com.cdsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataingestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataingestApplication.class, args);
+	}
+}
